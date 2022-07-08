26c43cf268b4a21d09797d63c666f01ce1dee276
pkosurilab <93301855+pkosurilab@users.noreply.github.com>
2022-07-08 12:55:14 -0700

    Add flatfield correction, flips, and crops to img utilities

--- utils/imutils.py
+++ utils/imutils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Callable, List, Union
 
+from PIL import Image as PILImage
 import numpy as np
 from napari.layers import Image
 from skimage.io import imread
@@ -10,17 +11,25 @@ ArrayLike = Union[
     np.ndarray, "dask.array.Array"
 ]  # could add other array types if needed
 
+def flatfield_correct(array:ArrayLike, corr) -> ArrayLike:
+    print("hello!")
+    return corr(array)
 
 def transpose(array: ArrayLike) -> ArrayLike:
     return array.T
 
+def flip_x(array: ArrayLike) -> ArrayLike:
+    return  np.flip(array, axis=1)
 
-def crop_black_border(array: ArrayLike, border_width: int = 12) -> ArrayLike:
+def flip_y(array: ArrayLike) -> ArrayLike:
+    return  np.flip(array, axis=0)
+
+def crop_black_border(array: ArrayLike, border_width: int = 40) -> ArrayLike:
     """
     Crops away the band of black pixels on the Nikon camera used in our lab.
     """
     right = -border_width if border_width > 0 else None
-    return array[:, :right]
+    return array[border_width:right, border_width:right]
 
 
 def subsample(array: ArrayLike, factor: int = 4, method="slice") -> ArrayLike:
@@ -46,3 +55,19 @@ def load_image(
         for t in transforms:
             img = t(img)
     return img
+
+def load_image_from_stack(
+    file: FilePath, frame: int = 0, transforms: List[Callable[[ArrayLike], ArrayLike]] = None
+) -> np.ndarray:
+    im = PILImage.open(file)
+    im.seek(frame) #move to selected frame
+    h,w = np.shape(im)
+    img = np.zeros((h,w))
+    img = np.array(im).astype(np.uint8) #extract frame
+    # if img.ndim == 2:
+    #    img = np.expand_dims(img, axis=0)
+    if transforms is not None:
+        for t in transforms:
+            img = t(img)
+    return img
+
